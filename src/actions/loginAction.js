eef8d644e1c8c9762d4496bae72b9da9ec45adc2
Yaswanthram-396 <yaswanthram.396@gmail.com>
2025-01-08 18:08:05 +0530

    commit un wanted reference files

--- src/actions/loginAction.js
+++ src/actions/loginAction.js
@@ -1,25 +1,25 @@
-export const loginUser = (username, password) => {
-  return async (dispatch) => {
-    dispatch({ type: "LOGIN_REQUEST" });
+// export const loginUser = (username, password) => {
+//   return async (dispatch) => {
+//     dispatch({ type: "LOGIN_REQUEST" });
 
-    try {
-      const response = await fetch("https://apis.ccbp.in/login", {
-        method: "POST",
-        body: JSON.stringify({ username, password }),
-      });
+//     try {
+//       const response = await fetch("https://apis.ccbp.in/login", {
+//         method: "POST",
+//         body: JSON.stringify({ username, password }),
+//       });
 
-      if (!response.ok) {
-        throw new Error("Login failed");
-      }
+//       if (!response.ok) {
+//         throw new Error("Login failed");
+//       }
 
-      const data = await response.json();
-      const token = data.jwt_token;
-      console.log(token);
-      dispatch({ type: "LOGIN_SUCCESS", payload: token });
-      return token;
-    } catch (error) {
-      console.log(username, password);
-      dispatch({ type: "LOGIN_FAILURE", error: true });
-    }
-  };
-};
+//       const data = await response.json();
+//       const token = data.jwt_token;
+//       console.log(token);
+//       dispatch({ type: "LOGIN_SUCCESS", payload: token });
+//       return token;
+//     } catch (error) {
+//       console.log(username, password);
+//       dispatch({ type: "LOGIN_FAILURE", error: true });
+//     }
+//   };
+// };
